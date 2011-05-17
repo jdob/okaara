a02aeac5c16845a0cccb4802e99fae4775e37969
Jay Dobies <jason.dobies@redhat.com>
2011-05-17 13:33:13 -0400

    Added more colors

--- src/okaara/prompt.py
+++ src/okaara/prompt.py
@@ -21,9 +21,32 @@ LOG = logging.getLogger(__name__)
 ABORT = object()
 
 COLOR_WHITE = '\033[0m'
-COLOR_GREEN = '\033[92m'
-COLOR_RED = '\033[91m'
-COLOR_YELLOW = '\033[93m'
+COLOR_BRIGHT_WHITE = '\033[1m'
+
+COLOR_GRAY = '\033[30m'
+COLOR_RED = '\033[31m'
+COLOR_GREEN = '\033[32m'
+COLOR_YELLOW = '\033[33m'
+COLOR_BLUE = '\033[34m'
+COLOR_PURPLE = '\033[35m'
+COLOR_CYAN = '\033[36m'
+
+COLOR_LIGHT_GRAY = '\033[90m'
+COLOR_LIGHT_RED = '\033[91m'
+COLOR_LIGHT_GREEN = '\033[92m'
+COLOR_LIGHT_YELLOW = '\033[93m'
+COLOR_LIGHT_BLUE = '\033[94m'
+COLOR_LIGHT_PURPLE = '\033[95m'
+COLOR_LIGHT_CYAN = '\033[96m'
+
+COLOR_BG_GRAY = '\033[40m'
+COLOR_BG_RED = '\033[41m'
+COLOR_BG_GREEN = '\033[42m'
+COLOR_BG_YELLOW = '\033[43m'
+COLOR_BG_BLUE = '\033[44m'
+COLOR_BG_PURPLE = '\033[45m'
+COLOR_BG_CYAN = '\033[46m'
+
 
 # -- classes ------------------------------------------------------------------
 
