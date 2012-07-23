418a583bd3e70302097283ccf10aa76552ebd2f7
Jay Dobies <jason.dobies@redhat.com>
2012-07-23 16:34:09 -0400

    Removed pointless layer between table and its config

--- src/okaara/table.py
+++ src/okaara/table.py
@@ -24,10 +24,17 @@ _ALL_WRAP_POLICIES = (WRAP_POLICY_TRUNCATE, WRAP_POLICY_WRAP)
 
 # -- classes ------------------------------------------------------------------
 
-class InvalidBag(Exception): pass
+class InvalidTableSettings(Exception): pass
 
-class TableBag(object):
-    def __init__(self, num_cols,
+class Table(object):
+    """
+    :param prompt:
+    :type  prompt: Prompt
+    """
+
+    def __init__(self,
+                 prompt,
+                 num_cols,
                  col_widths=None,
                  col_separator=' ',
                  table_max_width=None,
@@ -36,6 +43,9 @@ class TableBag(object):
                  header_color=None,
                  row_colors=None,
                  color_separators=True):
+        super(Table, self).__init__()
+
+        self.prompt = prompt
 
         self.num_cols = num_cols
 
@@ -51,15 +61,13 @@ class TableBag(object):
         self.row_colors = row_colors
         self.color_separators = color_separators
 
-        self.validate()
-
     def __setattr__(self, name, value):
         # Only start running validate on attribute changes, not the first addition.
         # The constructor will manually call validate after all of the initial
         # values are set.
         run_validate = hasattr(self, name)
 
-        super(TableBag, self).__setattr__(name, value)
+        super(Table, self).__setattr__(name, value)
 
         if run_validate:
             self.validate()
@@ -67,29 +75,15 @@ class TableBag(object):
     def validate(self):
 
         if self.num_cols < 1:
-            raise InvalidBag('Number of columns must be greater than 0')
+            raise InvalidTableSettings('Number of columns must be greater than 0')
 
         if self.wrap_policy not in _ALL_WRAP_POLICIES:
-            raise InvalidBag('Wrap policy must be one of the module constants')
+            raise InvalidTableSettings('Wrap policy must be one of the module constants')
 
         if self.col_widths is not None:
             max_column_width = reduce(lambda x, y: x + y, self.col_widths)
             if max_column_width > self.table_max_width:
-                raise InvalidBag('Sum of maximum column widths must be less than or equal to the table width')
-
-class Table(object):
-    """
-    :type prompt: Prompt
-
-    :type table_bag: TableBag
-    """
-
-    def __init__(self, prompt, table_bag):
-        super(Table, self).__init__()
-
-        self.prompt = prompt
-        self.table_bag = table_bag
-
+                raise InvalidTableSettings('Sum of maximum column widths must be less than or equal to the table width')
 
     def render(self, data, headers=None):
 
@@ -98,7 +92,7 @@ class Table(object):
 
         # Render the header information if specified
         if headers is not None:
-            self.render_headers(col_widths, headers, self.table_bag.header_color)
+            self.render_headers(col_widths, headers, self.header_color)
             self.render_header_divider(table_width)
 
         # Render each line
@@ -106,8 +100,8 @@ class Table(object):
             text_color = None
 
             # Alternate across each row color
-            if self.table_bag.row_colors is not None:
-                text_color = self.table_bag.row_colors[i % len(self.table_bag.row_colors)]
+            if self.row_colors is not None:
+                text_color = self.row_colors[i % len(self.row_colors)]
 
             self.render_line(col_widths, line, text_color)
 
@@ -117,7 +111,7 @@ class Table(object):
         self.render_line(col_widths, headers, text_color)
 
     def render_header_divider(self, table_width):
-        header_divider = self.table_bag.header_divider_tick * table_width
+        header_divider = self.header_divider_tick * table_width
         self.prompt.write(header_divider)
 
     def render_line(self, col_widths, line, text_color):
@@ -130,9 +124,9 @@ class Table(object):
             width = col_widths[i]
             text = line[i]
 
-            if self.table_bag.wrap_policy is WRAP_POLICY_TRUNCATE:
+            if self.wrap_policy is WRAP_POLICY_TRUNCATE:
                 text = text[0:width]
-            elif self.table_bag.wrap_policy is WRAP_POLICY_WRAP:
+            elif self.wrap_policy is WRAP_POLICY_WRAP:
                 wrapped = self.prompt.wrap(text, wrap_width=width)
 
                 split_lines = wrapped.split('\n')
@@ -144,15 +138,15 @@ class Table(object):
             text += ' ' * (width - len(text))
 
             # Color the text if specified (before the separator is added)
-            if text_color is not None and not self.table_bag.color_separators:
+            if text_color is not None and not self.color_separators:
                 text = self.prompt.color(text, text_color)
 
             # Tack on the column separator if not the last column
             if i < (len(col_widths) - 1):
-                text += self.table_bag.col_separator
+                text += self.col_separator
 
             # If the separators should be colored, do them now
-            if text_color is not None and self.table_bag.color_separators:
+            if text_color is not None and self.color_separators:
                 text = self.prompt.color(text, text_color)
 
             # Time to finally write the column to the screen
@@ -177,15 +171,15 @@ class Table(object):
                     text += ' ' * (col_widths[i] - len(text))
 
                 # Color the text if specified (before the separator is added)
-                if text_color is not None and not self.table_bag.color_separators:
+                if text_color is not None and not self.color_separators:
                     text = self.prompt.color(text, text_color)
 
                 # Add in the column separator
                 if i < (len(col_widths) - 1):
-                    text += self.table_bag.col_separator
+                    text += self.col_separator
 
                 # If the separators should be colored, do them now
-                if text_color is not None and self.table_bag.color_separators:
+                if text_color is not None and self.color_separators:
                     text = self.prompt.color(text, text_color)
 
                 self.prompt.write(text, new_line=False, skip_wrap=True)
@@ -201,23 +195,23 @@ class Table(object):
         """
 
         # First step is an expected table width
-        table_width = self.table_bag.table_max_width or self.prompt.terminal_size()[0]
+        table_width = self.table_max_width or self.prompt.terminal_size()[0]
 
         # Calculate the column widths if not specified
-        col_widths = self.table_bag.col_widths
+        col_widths = self.col_widths
 
         # If not specified, evenly divide across the table width and throw
         # out any extra space
         if col_widths is None:
-            each_col_width = table_width / (self.table_bag.num_cols + len(self.table_bag.col_separator))
-            col_widths = [each_col_width for i in range(0, self.table_bag.num_cols)]
+            each_col_width = table_width / (self.num_cols + len(self.col_separator))
+            col_widths = [each_col_width for i in range(0, self.num_cols)]
 
         # If the table width is greater than the total width of the columns,
         # reduce the table width so it looks nicer
         total_col_width = reduce(lambda x, y: x + y, col_widths)
 
         # Add in the width of the separators
-        total_col_width += len(self.table_bag.col_separator) * (len(col_widths) - 1)
+        total_col_width += len(self.col_separator) * (len(col_widths) - 1)
         table_width = min(table_width, total_col_width)
 
         return table_width, col_widths
@@ -227,8 +221,7 @@ if __name__ == '__main__':
     import okaara.prompt
     p = okaara.prompt.Prompt()
 
-    bag = TableBag(4, table_max_width=60, wrap_policy=WRAP_POLICY_WRAP, col_separator=' | ')
-    table = Table(p, bag)
+    table = Table(p, 4, table_max_width=60, wrap_policy=WRAP_POLICY_WRAP, col_separator=' ')
 
     data = [
         ['aaaaa', 'bbbbb', 'ccccc', 'ddddd'],
@@ -245,15 +238,23 @@ if __name__ == '__main__':
     p.write('')
     p.write('')
 
-    bag.header_color = okaara.prompt.COLOR_BG_BLUE
-    bag.row_colors = [okaara.prompt.COLOR_BG_CYAN + okaara.prompt.COLOR_BLUE, okaara.prompt.COLOR_LIGHT_PURPLE, okaara.prompt.COLOR_CYAN]
-    bag.color_separators = True
+    table.col_separator = ' | '
+    table.table_max_width = 70
 
     table.render(data, headers=headers)
 
     p.write('')
     p.write('')
 
-    bag.color_separators = False
+    table.header_color = okaara.prompt.COLOR_BG_BLUE
+    table.row_colors = [okaara.prompt.COLOR_BG_CYAN + okaara.prompt.COLOR_BLUE, okaara.prompt.COLOR_LIGHT_PURPLE, okaara.prompt.COLOR_CYAN]
+    table.color_separators = True
+
+    table.render(data, headers=headers)
+
+    p.write('')
+    p.write('')
+
+    table.color_separators = False
 
     table.render(data, headers=headers)
