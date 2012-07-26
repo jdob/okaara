5a5ccd77b05dd2b1491b7ae64bdd8aa2297610ba
Jay Dobies <jason.dobies@redhat.com>
2012-07-26 17:05:42 -0400

    Added column alignment capabilities

--- samples/sample_table.py
+++ samples/sample_table.py
@@ -12,40 +12,110 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
 import okaara.prompt
-import okaara.table
+from okaara.table import Table, WRAP_POLICY_WRAP, ALIGN_RIGHT, ALIGN_LEFT, ALIGN_CENTER
 
-p = okaara.prompt.Prompt()
+# -----------------------------------------------------------------------------
 
-table = okaara.table.Table(p, 4, table_width=60, wrap_policy=okaara.table.WRAP_POLICY_WRAP, col_separator=' ')
-
-data = [
-    ['aaaaa', 'bbbbb', 'ccccc', 'ddddd'],
-    ['eeeeeeee', 'ffff', 'gggggggg', 'hhhhhhhhhhhhhhh'],
-    ['iiii', 'jjjj', 'kk', 'lllllllllllll'],
-    ['m', 'n', 'ooo', 'ppppppp'],
-    ['qqqqqq', 'rrrr', 'sss', 't']
+TEST_DATA = [
+    ['1', 'Entry 1', 'Lorem ipsum dolor sit amet,'],
+    ['2', 'Entry 2', 'consectetur adipisicing'],
+    ['3', 'Entry 3', 'elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'],
+    ['4', 'Entry 4', 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
+    ['5', 'Entry 5', 'Duis aute irure dolor'],
 ]
+TEST_HEADERS = ['ID', 'Title', 'Description']
 
-headers = ['Column 1', 'Column 2', 'Really Long Column 3','Column 4']
+NUM_COLS = len(TEST_HEADERS)
 
-table.render(data, headers=headers)
+PROMPT = okaara.prompt.Prompt()
 
-p.write('')
-p.write('')
+# -----------------------------------------------------------------------------
 
-table.col_separator = ' | '
-table.render(data, headers=headers)
+def main():
+    mappings = [
+        ['Table defaults: no column separator, truncate wrap policy, table the width of the terminal', basic],
+        ['Smaller table width with cell wrap policy set to wrap', wrapped],
+        ['Custom formatting for header divider and column separator', custom_formatting],
+        ['Alternating colored rows and header row colors', colored],
+        ['Table defaults, no header data specified', no_headers],
+        ['First column is right aligned, second center aligned, custom column widths', alignments],
+        ['Header columns centered', header_alignments],
+    ]
 
-p.write('')
-p.write('')
+    menu_values = [m[0] for m in mappings]
+    selected = PROMPT.prompt_menu('Select the table to demonstrate:', menu_values=menu_values)
+    PROMPT.write('')
 
-table.header_color=okaara.prompt.COLOR_BG_BLUE
-table.row_colors=[okaara.prompt.COLOR_BG_CYAN + okaara.prompt.COLOR_BLUE, okaara.prompt.COLOR_LIGHT_PURPLE, okaara.prompt.COLOR_CYAN]
-table.color_separators=True
-table.render(data, headers=headers)
+    if selected is okaara.prompt.ABORT:
+        return
 
-p.write('')
-p.write('')
+    func = mappings[selected][1]
+    func()
 
-table.color_separators=False
-table.render(data, headers=headers)
+# -- examples -----------------------------------------------------------------
+
+def basic():
+    PROMPT.write('Table rendered using the defaults.')
+    PROMPT.write('')
+
+    table = Table(PROMPT, NUM_COLS)
+    table.render(TEST_DATA, headers=TEST_HEADERS)
+
+def wrapped():
+    PROMPT.write('Smaller table width with cell wrap policy set to wrap.')
+    PROMPT.write('')
+
+    table = Table(PROMPT, NUM_COLS, table_width=60, wrap_policy=WRAP_POLICY_WRAP)
+    table.render(TEST_DATA, headers=TEST_HEADERS)
+
+def custom_formatting():
+    PROMPT.write('Custom formatting for header divider and column separator')
+    PROMPT.write('')
+
+    table = Table(PROMPT, NUM_COLS, table_width=60, wrap_policy=WRAP_POLICY_WRAP, header_divider_tick='*', col_separator=' | ')
+    table.render(TEST_DATA, headers=TEST_HEADERS)
+
+def colored():
+    PROMPT.write('Alternating colored rows and header row colors')
+    PROMPT.write('')
+
+    table = Table(PROMPT, NUM_COLS, table_width=60, wrap_policy=WRAP_POLICY_WRAP)
+    table.header_color=okaara.prompt.COLOR_BG_BLUE
+    table.row_colors=[okaara.prompt.COLOR_LIGHT_BLUE, okaara.prompt.COLOR_LIGHT_PURPLE, okaara.prompt.COLOR_CYAN]
+
+    table.render(TEST_DATA, headers=TEST_HEADERS)
+
+def no_headers():
+    PROMPT.write('Table defaults, no header data specified')
+    PROMPT.write('')
+
+    table = Table(PROMPT, NUM_COLS)
+    table.render(TEST_DATA)
+
+def alignments():
+    PROMPT.write('First column is right aligned, second center aligned, custom column widths')
+    PROMPT.write('')
+
+    alignments = [ALIGN_LEFT for i in range(0, NUM_COLS)]
+    alignments[0] = ALIGN_RIGHT
+    alignments[1] = ALIGN_CENTER
+
+    widths = [20 for i in range(0, NUM_COLS)]
+    widths[0] = 5
+
+    table = Table(PROMPT, NUM_COLS, col_alignments=alignments, col_widths=widths, wrap_policy=WRAP_POLICY_WRAP)
+    table.render(TEST_DATA, headers=TEST_HEADERS)
+
+def header_alignments():
+    PROMPT.write('Header columns centered')
+    PROMPT.write('')
+
+    alignments = [ALIGN_CENTER for i in range(0, NUM_COLS)]
+
+    table = Table(PROMPT, NUM_COLS, table_width=60, wrap_policy=WRAP_POLICY_WRAP, header_col_alignments=alignments)
+    table.render(TEST_DATA, headers=TEST_HEADERS)
+
+# -----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    main()
--- src/okaara/table.py
+++ src/okaara/table.py
@@ -16,13 +16,20 @@ import copy
 # -- constants ----------------------------------------------------------------
 
 # Causes values in a column to be truncated if they exceed the col width
-WRAP_POLICY_TRUNCATE = object()
+WRAP_POLICY_TRUNCATE = 'wrap-truncate'
 
 # Causes values in a column to wrap to a second line if they exceed the col width
-WRAP_POLICY_WRAP = object()
+WRAP_POLICY_WRAP = 'wrap-wrap'
 
 # Internal utility
 _ALL_WRAP_POLICIES = (WRAP_POLICY_TRUNCATE, WRAP_POLICY_WRAP)
+
+# Column alignment constants
+ALIGN_LEFT = 'align-left'
+ALIGN_RIGHT = 'align-right'
+ALIGN_CENTER = 'align-center'
+
+_ALL_ALIGNMENTS = (ALIGN_LEFT, ALIGN_RIGHT, ALIGN_CENTER)
 
 # -- classes ------------------------------------------------------------------
 
@@ -38,6 +45,8 @@ class Table(object):
                  prompt,
                  num_cols,
                  col_widths=None,
+                 col_alignments=None,
+                 header_col_alignments=None,
                  col_separator=' ',
                  table_width=None,
                  wrap_policy=WRAP_POLICY_TRUNCATE,
@@ -58,18 +67,17 @@ class Table(object):
 
         # Look & Feel
         self.col_separator = col_separator
+        self.col_alignments = col_alignments
+        self.header_col_alignments = header_col_alignments
         self.header_divider_tick = header_divider_tick
         self.header_color = header_color
         self.row_colors = row_colors
         self.color_separators = color_separators
 
-        # Make sure the values are sane
-        self.validate()
-
     def __str__(self):
         return 'table_max_width [%s] col_widths [%s]' % (self.table_width, self.col_widths)
 
-    def validate(self):
+    def validate(self, table_width, col_widths):
 
         if self.num_cols < 1:
             raise InvalidTableSettings('Number of columns must be greater than 0')
@@ -77,13 +85,29 @@ class Table(object):
         if self.wrap_policy not in _ALL_WRAP_POLICIES:
             raise InvalidTableSettings('Wrap policy must be one of the module constants')
 
-        if self.col_widths is not None:
-            max_column_width = reduce(lambda x, y: x + y, self.col_widths)
-            if max_column_width > self.table_width:
-                raise InvalidTableSettings('Sum of maximum column widths must be less than or equal to the table width')
+        if self.col_alignments is not None:
+            illegal_alignments = [a for a in self.col_alignments if a not in _ALL_ALIGNMENTS]
+            if len(illegal_alignments) > 0:
+                raise InvalidTableSettings('Column alignment values must be one of the module constants')
 
-            if self.num_cols != len(self.col_widths):
-                raise InvalidTableSettings('Number of columns [%s] must equal the number of column widths specified [%s]' % (self.num_cols, len(self.col_widths)))
+            if len(self.col_alignments) != self.num_cols:
+                raise InvalidTableSettings('One column alignment must be specified for each column')
+
+        if self.header_col_alignments is not None:
+            illegal_alignments = [a for a in self.header_col_alignments if a not in _ALL_ALIGNMENTS]
+            if len(illegal_alignments) > 0:
+                raise InvalidTableSettings('Header column alignment values must be one of the module constants')
+
+            if len(self.header_col_alignments) != self.num_cols:
+                raise InvalidTableSettings('One header column alignment must be specified for each column')
+
+        max_column_width = reduce(lambda x, y: x + y, col_widths)
+
+        if max_column_width > table_width:
+            raise InvalidTableSettings('Sum of maximum column widths must be less than or equal to the table width')
+
+        if self.num_cols != len(col_widths):
+            raise InvalidTableSettings('Number of columns [%s] must equal the number of column widths specified [%s]' % (self.num_cols, len(self.col_widths)))
 
     # -- public ---------------------------------------------------------------
 
@@ -91,7 +115,7 @@ class Table(object):
 
         # Recalculate and revalidate
         table_width, col_widths = self.calculate_widths()
-        self.validate()
+        self.validate(table_width, col_widths)
 
         # Render the header information if specified
         if headers is not None:
@@ -109,7 +133,7 @@ class Table(object):
             if self.row_colors is not None:
                 text_color = self.row_colors[row_num % len(self.row_colors)]
 
-            self.render_row(line, col_widths, text_color)
+            self.render_row(line, col_widths, text_color, self.col_alignments)
             self.render_row_divider(table_width, row_num)
 
     # -- render pieces --------------------------------------------------------
@@ -117,13 +141,13 @@ class Table(object):
     def render_headers(self, headers, col_widths, text_color):
         header_cells = self.parse_cells([headers], col_widths)
 
-        self.render_row(header_cells[0], col_widths, text_color)
+        self.render_row(header_cells[0], col_widths, text_color, self.header_col_alignments)
 
     def render_header_divider(self, table_width):
         header_divider = self.header_divider_tick * table_width
         self.prompt.write(header_divider)
 
-    def render_row(self, row_cells, col_widths, text_color):
+    def render_row(self, row_cells, col_widths, text_color, col_alignments):
 
         # Going to pop elements out of the cells, so copy them first
         row_cells = copy.deepcopy(row_cells)
@@ -141,9 +165,24 @@ class Table(object):
                 else:
                     text = cell.pop_line()
 
-                    # Pad the right side of the text with enough spaces to line up
-                    # the next column correctly
-                    text += ' ' * (width - len(text))
+                    # Align the column correctly
+                    alignment = ALIGN_LEFT
+                    if col_alignments is not None:
+                        alignment = col_alignments[i]
+
+                    padding = ' ' * (width - len(text))
+                    if alignment == ALIGN_LEFT:
+                        text += padding
+                    elif alignment == ALIGN_RIGHT:
+                        text = padding + text
+                    else:
+                        left_padding_count = (width - len(text)) / 2
+                        right_padding_count = (width - len(text)) - left_padding_count
+
+                        left_padding = ' ' * left_padding_count
+                        right_padding = ' ' * right_padding_count
+
+                        text = left_padding + text + right_padding
 
                     # Color the text if specified (before the separator is added)
                     if text_color is not None and not self.color_separators:
@@ -225,11 +264,11 @@ class Table(object):
 
                 # Apply the wrap policy to transform the text
 
-                if self.wrap_policy is WRAP_POLICY_TRUNCATE:
+                if self.wrap_policy == WRAP_POLICY_TRUNCATE:
                     text = text[0:col_width]
                     cell.add_line(text)
 
-                elif self.wrap_policy is WRAP_POLICY_WRAP:
+                elif self.wrap_policy == WRAP_POLICY_WRAP:
                     wrapped = self.prompt.wrap(text, wrap_width=col_width)
 
                     split_lines = wrapped.split('\n')
