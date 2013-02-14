7d61010e1ecc56dbe36717fd48fca835c770605e
Jay Dobies <jason.dobies@redhat.com>
2013-02-14 16:39:05 -0500

    Automatic commit of package [python-okaara] release [1.0.30-1].

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='okaara',
-        version='1.0.29',
+        version='1.0.30',
         description='Python command line utilities',
         url='http://jdob.fedorapeople.org/okaara/',
         license='GPLv2',
