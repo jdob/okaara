79f4d4e885029b3a9e9754f398dd724b0fb48dd1
Jay Dobies <jason.dobies@redhat.com>
2013-05-23 10:06:19 -0400

    Added copyright information

--- okaara/shell.py
+++ okaara/shell.py
@@ -1,10 +1,17 @@
-# This software is licensed to you under the GNU General Public License,
-# version 2 (GPLv2). There is NO WARRANTY for this software, express or
-# implied, including the implied warranties of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
-# along with this software; if not, see
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
-
+# Copyright (c) 2011-2013 Jason Dobies
+#
+# This file is part of Okaara.
+#
+# Okaara is free software: you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation, either version 3
+# of the License, or (at your option) any later version.
+#
+# Okaara is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with Okaara.
+# If not, see <http://www.gnu.org/licenses/>.
 
 import gettext
 import logging
